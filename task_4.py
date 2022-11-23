b9f0ee7da83751aa5fc0a3151d7fbe09ab2dd448
MilenaPoleno <112932828+MilenaPoleno@users.noreply.github.com>
2022-11-23 15:55:01 +0300

    Create task_4.py

---
+++ task_4.py
@@ -0,0 +1,11 @@
+"""4) Пользователь вводит целое положительное число. Найдите самую большую цифру в числе. Для решения используйте цикл while и арифметические операции."""
+
+n = int(input("Введите целое положительное число: "))
+all_numbers_of_n = []
+while n > 0:
+  number = n % 10
+  all_numbers_of_n.append(number)
+  n = n // 10
+
+max_number = max(all_numbers_of_n)
+print(max_number)
