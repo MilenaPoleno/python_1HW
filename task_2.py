f1b45df6717c30b006c237e09a364e7c181ccee7
MilenaPoleno <112932828+MilenaPoleno@users.noreply.github.com>
2022-11-24 11:11:06 +0300

    поправлены отступы

--- task_2.py
+++ task_2.py
@@ -7,10 +7,10 @@ time_seconds = time_all - time_hours * 3600 - time_minutes * 60
 string_0 = str(0)
 
 if time_hours < 10:
-  time_hours = string_0 + str(time_hours)
+    time_hours = string_0 + str(time_hours)
 if time_minutes < 10:
-  time_minutes = string_0 + str(time_minutes)
+    time_minutes = string_0 + str(time_minutes)
 if time_seconds < 10:
-  time_seconds = string_0 + str(time_seconds)
+    time_seconds = string_0 + str(time_seconds)
 
 print(f"Время в формате чч:мм:сс - {time_hours}:{time_minutes}:{time_seconds}")
